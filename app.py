e7a6db7efb7232776f86a242c40356b3c76d4dd3
IfalEXZ <ifaltampan69@gmail.com>
2024-10-17 12:22:25 +0800

    get ready

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, request, send_file
 from flask_cors import CORS
 from rembg import remove
@@ -8,7 +7,7 @@ import io
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/remove-background', methods=['POST'])
+@app.route('/api/remove-background', methods=['POST'])
 def remove_background():
     if 'image' not in request.files:
         return 'No image file uploaded', 400
@@ -29,5 +28,4 @@ def remove_background():
     return send_file(img_byte_arr, mimetype='image/png')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
